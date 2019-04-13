22ee345605357c0a80d1bfcaf5704c4bd516ab39
Gabriel Tavolaro <38760792+gabrieltavolaro@users.noreply.github.com>
2019-04-13 17:08:41 -0300

    Create festifyboot.js

---
+++ festifyboot.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://festify.us/party/-LcMDRlQDBR9Cyqtu59w
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(function(){
+
+        //fetch('http://0.0.0.0:8000/music.txt').then(x => x.text()).then(j => console.log(j));
+
+        var musica = "Cheap Thrills";
+
+        var lista = document.querySelector('body > app-shell').shadowRoot.querySelector('view-party').shadowRoot.querySelector('app-drawer-layout > div > iron-pages > party-queue').shadowRoot.querySelector('dom-flip').querySelectorAll('party-track')
+
+        for (var x of lista) {
+
+            var titulo = x.shadowRoot.querySelector('div.metadata-wrapper > h2');
+            console.log(titulo.innerText);
+
+            if ( musica == titulo.innerText ) {
+                x.shadowRoot.querySelector('div.icon-wrapper paper-icon-button').click();
+                console.log('**ACHEI**');
+
+                indexedDB.deleteDatabase('firebaseLocalStorageDb');
+                location.reload();
+
+                break;
+
+            }
+
+        }
+
+        
+
+    }, 6000);
+
+
+})();
